09df8096f906db2705227d4d714256136c5e453d
Koose <KooseTheBlogger@gmail.com>
2022-02-14 20:00:55 +0000

    net working gtting refrofit to work

--- app/src/main/java/com/koose/monsterdataretroif/data/MonsterRespository.kt
+++ app/src/main/java/com/koose/monsterdataretroif/data/MonsterRespository.kt
@@ -2,11 +2,28 @@ package com.koose.monsterdataretroif.data
 
 import android.app.Application
 import android.net.ConnectivityManager
+import androidx.annotation.WorkerThread
+import com.koose.monsterdataretroif.utlis.WEB_SERVICE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
-class MonsterRespository (app: Application){
+class MonsterRespository (val app: Application){
 
 
 
+    //There's, Network in the device
+    @WorkerThread
+    suspend fun callWebService() {
+        if (networkAvailable()) {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(WEB_SERVICE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+            val service = retrofit.create(MonsterService::class.java)
+            val serviceData = service.getMonsterData().body() ?: emptyList()
+            monsterData.postValue(serviceData)
+        }
+    }
 
 
 
