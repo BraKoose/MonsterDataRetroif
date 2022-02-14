6d7af952b14e02d01f39d79d2f71a850447cabdd
Koose <KooseTheBlogger@gmail.com>
2022-02-14 19:57:13 +0000

    internet permission

--- app/src/main/java/com/koose/monsterdataretroif/data/MonsterRespository.kt
+++ app/src/main/java/com/koose/monsterdataretroif/data/MonsterRespository.kt
@@ -1,4 +1,22 @@
 package com.koose.monsterdataretroif.data
 
-class MonsterRespository {
+import android.app.Application
+import android.net.ConnectivityManager
+
+class MonsterRespository (app: Application){
+
+
+
+
+
+
+
+
+    @Suppress("DEPRECATION")
+    private fun networkAvailable(): Boolean {
+        val connectivityManager = app.getSystemService(Context.CONNECTIVITY_SERVICE)
+                as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo?.isConnectedOrConnecting ?: false
+    }
 }
