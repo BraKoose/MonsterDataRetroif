e4b994ebb2e630c840e6d9e2b5a9eb2fbe84958b
Koose <KooseTheBlogger@gmail.com>
2022-02-14 19:16:56 +0000

    data class

--- app/src/main/java/com/koose/monsterdataretroif/MainActivity.kt
+++ app/src/main/java/com/koose/monsterdataretroif/MainActivity.kt
@@ -2,10 +2,13 @@ package com.koose.monsterdataretroif
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.inflate
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/koose/monsterdataretroif/data/MonsterData.kt
@@ -0,0 +1,10 @@
+package com.koose.monsterdataretroif.data
+
+data class MonsterData(
+    val MonsterName: String,
+    val imageFile: String,
+    val caption:String,
+    val description:String,
+    val price : Double,
+    val scariness: Int
+)
