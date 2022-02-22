7ec3090cb2b973fc9b1d3201fc863169b72575a7
Koose <KooseTheBlogger@gmail.com>
2022-02-22 13:41:26 +0000

    grid, background, and selector

--- app/src/main/java/com/koose/monsterdataretroif/data/Adapter/MonsterAdapter.kt
+++ app/src/main/java/com/koose/monsterdataretroif/data/Adapter/MonsterAdapter.kt
@@ -1,25 +1,32 @@
 package com.koose.monsterdataretroif.data.Adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.koose.monsterdataretroif.R
+import com.koose.monsterdataretroif.data.MonsterData
 
-class MonsterAdapter(): RecyclerView.Adapter<MonsterAdapter.ViewHolder>(){
+class MonsterAdapter( val context: Context,
+                      val monstList: List<MonsterData>
+): RecyclerView.Adapter<MonsterAdapter.ViewHolder>(){
 
-
-
-    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+    override fun getItemCount() = monstList.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.monster_grid_items, parent, false)
+
+        return ViewHolder(view )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
     }
 
 }
