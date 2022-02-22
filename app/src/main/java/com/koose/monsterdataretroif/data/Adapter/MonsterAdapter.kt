e65efe829df231b3f02b1dafd4e6a2d4d56a9779
Koose <KooseTheBlogger@gmail.com>
2022-02-22 12:39:48 +0000

    members implement on the Monster Adapter

--- app/src/main/java/com/koose/monsterdataretroif/data/Adapter/MonsterAdapter.kt
+++ app/src/main/java/com/koose/monsterdataretroif/data/Adapter/MonsterAdapter.kt
@@ -1,2 +1,25 @@
 package com.koose.monsterdataretroif.data.Adapter
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MonsterAdapter(): RecyclerView.Adapter<MonsterAdapter.ViewHolder>(){
+
+
+
+    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
